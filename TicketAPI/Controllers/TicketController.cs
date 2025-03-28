e1079a4fb263631ab930b7abf86ac1d9f710e813
HingleMcCringle25 <adriandemont11@gmail.com>
2025-03-28 17:03:10 -0300

    Add project files.

---
+++ TicketAPI/Controllers/TicketController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TicketAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        private readonly ILogger<TicketController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public TicketController(ILogger<TicketController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from TicketController!");
+        }
+
+        [HttpPost]
+        public IActionResult Post(Ticket ticket)
+        {
+            //validate ticket
+            if (string.IsNullOrEmpty(ticket.FirstName))
+            {
+                return BadRequest("Invalid first name");
+            }
+            if (string.IsNullOrEmpty(ticket.LastName))
+            {
+                return BadRequest("Invalid last name");
+            }
+
+            return Ok("Hello " + ticket.FirstName + " " + ticket.LastName + " from TicketController!");
+        }
+    }
+}
---
+++ TicketAPI/Ticket.cs
@@ -0,0 +1,8 @@
+﻿namespace TicketAPI
+{
+    public class Ticket
+    {
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+    }
+}
